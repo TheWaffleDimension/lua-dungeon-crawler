178024bd13e446bd77dd784cdfe27dd47df86d0b
TheWaffleDimension <thewaffledimension@gmail.com>
2017-11-01 17:03:11 -0500

    Added an easy scaling variable, and modified the code to suit.
    The game has stuttering issues now, I've noticed. Working on it. :D

--- main.lua
+++ main.lua
@@ -4,13 +4,15 @@ camera = require"classes.camera"
 helper = require"lib.helper"
 sti = require"lib.sti"
 
+scale = 1
 tileSize = 32
 
 local controls = {up = 0, down = 0, left = 0, right = 0}
 --local tiles = {{name = "solid", x = _G.tileSize, y = 0, w = _G.tileSize, h = _G.tileSize}}
 
 function love.load()
-	world = bump.newWorld(32)
+	camera:scale(scale, scale)
+	world = bump.newWorld(tileSize)
 	player:init(nil, 0, 0)
 	world:add(player, player.x, player.y, _G.tileSize, _G.tileSize)
 	--world:add(tiles[1], tiles[1].x, tiles[1].y, tiles[1].w, tiles[1].h)
@@ -18,7 +20,7 @@ function love.load()
 	
 	for i,v in pairs(map.tiles) do
 		if v.properties.solid == true then
-			world:add(v, v.offset.x, v.offset.y, width, height)
+			world:add(v, v.offset.x, v.offset.y, v.width, v.height)
 		end
 	end
 end
@@ -29,17 +31,18 @@ function love.update(dt)
 	player.vx = xDir*player.speed
 	player.vy = yDir*player.speed
 	player:update(dt)
-	local camX = (camera.x - player.x + love.graphics.getHeight()*0.5 + _G.tileSize) * dt * camera.speed
-	local camY = (camera.y - player.y + love.graphics.getHeight()*0.5 + _G.tileSize) * dt * camera.speed
+	local camX = (camera.x - player.x*scale + love.graphics.getHeight()*0.5*scale + tileSize*2*scale) * dt * camera.speed
+	local camY = (camera.y - player.y*scale + love.graphics.getHeight()*0.5*scale - tileSize*0.5*scale) * dt * camera.speed
 	camera:move(-1*camX, -1*camY)
 	map:update(dt)
 end
 
 function love.draw()
-	map:draw(-camera.x, -camera.y, 2*camera.scaleX, 2*camera.scaleY)
+	love.graphics.setColor( 255, 255, 255 )
+	map:draw(-camera.x, -camera.y, 2, 2)
 	camera:set()
 	love.graphics.setColor( 10, 255, 10, 0.9*255 )
-	--love.graphics.rectangle("fill", _G.tileSize, 0, _G.tileSize, _G.tileSize)
+	--love.graphics.rectangle("fill", tileSize, 0, tileSize, tileSize)
 	player:draw()
 	camera:unset()
 end
